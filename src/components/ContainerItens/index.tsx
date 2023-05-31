6a174a092d065cd87cbc0960716eb264c06c3dc2
marcelo.almeida <90574339+MarceloALMoreira@users.noreply.github.com>
2023-05-31 17:36:15 -0300

    feat component title

---
+++ src/components/ContainerItens/index.tsx
@@ -0,0 +1,10 @@
+import { ContainerItens as Container } from "./styles"
+
+
+
+function ContainerItens({ children, isBlur }) {
+    return
+    <Container isBlur={isBlur}>{children}</Container>
+}
+
+export default ContainerItens
